a4f2e47ec3a7df5046cd458b7a789e37fbb874b6
silvade <myanova13@gmail.com>
2023-05-31 22:57:03 +0600

    PEP8 edits

--- test_items.py
+++ test_items.py
@@ -3,9 +3,10 @@ from selenium.webdriver.common.by import By
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_add_to_basket_button(browser):
+
+def test_button_add_to_basket(browser):
     browser.get(link)
     time.sleep(30)
     addButtons = browser.find_elements(By.CLASS_NAME, "btn-add-to-basket")
     countOfAddButtons = len(addButtons)
-    assert countOfAddButtons == 1, f'Expected one of add to basket button, but found {countOfAddButtons} buttons.'
+    assert countOfAddButtons == 1, f'Expected one add to basket button, but found {countOfAddButtons} buttons.'
